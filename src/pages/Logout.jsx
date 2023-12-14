47f357c88a7b42b91f9ca12f2331e927a6136388
Rodrigo França <rodrigolopes2005@hotmail.com>
2023-12-13 21:23:03 -0300

    first commit

---
+++ src/pages/Logout.jsx
@@ -0,0 +1,9 @@
+export default function Logout() {
+    localStorage.removeItem('usuarioLogado');
+    location.assign('/');
+    return (
+        <main>
+            <h1>Logout</h1>
+        </main>
+    );
+}
